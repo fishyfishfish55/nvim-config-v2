d03879d751fda01d81666e3b5bd122b5c8acdbf8
fishyfishfish55 <l.lacossct@gmail.com>
2025-12-27 15:07:52 -0500

    add gitignore

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -14,26 +14,40 @@ vim.api.nvim_create_autocmd("LspAttach", {
     if ok then
       wk.add({
         { "<leader>l",  group = "+lsp" },
-        { "<leader>ld", vim.lsp.buf.definition,     desc = "Go to defenition",     mode = "n" },
-        { "<leader>lr", vim.lsp.buf.references,     desc = "Go to reference",      mode = "n" },
-        { "<leader>li", vim.lsp.buf.implementation, desc = "Go to implementation", mode = "n" },
+        { "<leader>ld", vim.lsp.buf.definition,     desc = "Go to definition", },
+        { "<leader>lr", vim.lsp.buf.references,     desc = "Go to reference", },
+        { "<leader>li", vim.lsp.buf.implementation, desc = "Go to implementation", },
 
-        { "<leader>ln", vim.lsp.buf.rename,         desc = "Rename symbol",        mode = "n" },
-        { "<leader>la", vim.lsp.buf.code_action,    desc = "Code action",          mode = "n" },
+        { "<leader>ln", vim.lsp.buf.rename,         desc = "Rename symbol", },
+        { "<leader>la", vim.lsp.buf.code_action,    desc = "Code action", },
 
-        { "<leader>le", vim.diagnostic.open_float,  desc = "Diagnostics (float)",  mode = "n" },
-        { "<leader>l[", vim.diagnostic.goto_prev,   desc = "Previous diagnostic",  mode = "n" },
-        { "<leader>l]", vim.diagnostic.goto_next,   desc = "Next diagnostic",      mode = "n" },
+        { "<leader>le", vim.diagnostic.open_float,  desc = "Diagnostics (float)", },
+        { "<leader>l[", vim.diagnostic.goto_prev,   desc = "Previous diagnostic", },
+        { "<leader>l]", vim.diagnostic.goto_next,   desc = "Next diagnostic", },
 
-        { "K",          vim.lsp.buf.hover,          desc = "Hover documentation",  mode = "n" },
+        { "K",          vim.lsp.buf.hover,          desc = "Hover documentation", },
 
+      }, {
+        buffer = event.buf,
+        mode = "n",
       })
       local fmt_ok, fmt = pcall(require, "utils.format")
       if fmt_ok then
         wk.add({
-          { "<leader>lf", fmt.format, desc = "Format", mode = "n" },
+          { "<leader>lf", fmt.format, desc = "Format", mode = "n", buffer = event.buf },
         })
       end
+    end
+
+    -- Format on save (buffer-local)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = event.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = event.buf })
+        end,
+      })
     end
   end,
 })
