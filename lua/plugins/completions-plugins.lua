ce3b3da237e01bc73ce90059563656b46f271cad
fishyfishfish55 <l.lacossct@gmail.com>
2025-12-27 23:12:24 -0500

    fix broken blink.cmp config

--- lua/plugins/completions-plugins.lua
+++ lua/plugins/completions-plugins.lua
@@ -3,13 +3,19 @@ return {
     "saghen/blink.cmp",
     event = "InsertEnter",
     version = "1.*",
+    dependencies = {
+      { "rafamadriz/friendly-snippets" }
+    },
     opts = {
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
-    },
-    keymap = {
-      preset = "default",
+      keymap = {
+        preset = "super-tab",
+      },
+      completion = {
+        documentation = { auto_show = true },
+      },
     },
   },
 }
