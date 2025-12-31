081f68542d71e1b245d3d1f0ccdd14dbcb099c7d
fishyfishfish55 <l.lacossct@gmail.com>
2025-12-31 10:49:39 -0500

    manually enable language servers

--- lua/langs/python.lua
+++ lua/langs/python.lua
@@ -2,6 +2,8 @@
 local conform = require("conform")
 local lint = require("lint")
 
+vim.lsp.enable("basedpyright")
+
 -- formatter preference order
 conform.formatters_by_ft.python = {
 	"isort",
--- lua/plugins/lsp-plugins.lua
+++ lua/plugins/lsp-plugins.lua
@@ -1,7 +1,9 @@
 return {
 	{
 		"mason-org/mason-lspconfig.nvim",
-		opts = {},
+		opts = {
+			automatic_enable = false,
+		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
